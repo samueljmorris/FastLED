93b6e5818930a610a334d79173f67d43a2839503
Samuel Joseph Morris <32405210+samueljmorris@users.noreply.github.com>
2020-02-25 23:16:07 -0500

    Basic IO test passed

--- examples/TreeOfDeath/UpdatedReactivity/DemoReel100.ino
+++ examples/TreeOfDeath/UpdatedReactivity/DemoReel100.ino
@@ -28,8 +28,9 @@ CRGB leds[NUM_LEDS];
 //255 red
 
 void setup() {
-    //init proximity sensor, pin 3???
-    pinMode(3, INPUT);
+    Serial.begin(115200);
+    //init proximity sensor, pin 2
+    pinMode(2, INPUT);
     //init mic, analog a0
     delay(100); // 3 second delay for recovery
   
@@ -40,64 +41,71 @@ void setup() {
   FastLED.setBrightness(BRIGHTNESS);
 }
 
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { confetti, sinelon, juggle };
+// // List of patterns to cycle through.  Each is defined as a separate function below.
+// typedef void (*SimplePatternList[])();
+// SimplePatternList gPatterns = { confetti, sinelon, juggle };
 
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+// uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+// uint8_t gHue = 0; // rotating "base color" used by many of the patterns
   
 void loop()
 {
-  boolean pir = digitalRead(3); //Is there a person nearby?
-  
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+  boolean pir = digitalRead(2); //Is there a person nearby?
+  int soundLevel = analogRead(A0); //Current mic level?
+  EVERY_N_MILLISECONDS(100){
+    Serial.print("PIR Sensor state: ");
+    Serial.println(pir);
+    Serial.print("Raw sound level: ");
+    Serial.println(soundLevel);
+  }
 
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
+  // // Call the current pattern function once, updating the 'leds' array
+  // gPatterns[gCurrentPatternNumber]();
+
+  // // send the 'leds' array out to the actual LED strip
+  // FastLED.show();  
+  // // insert a delay to keep the framerate modest
+  // FastLED.delay(1000/FRAMES_PER_SECOND); 
 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 10 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 5 ) { nextPattern(); } // change patterns periodically
+  // EVERY_N_MILLISECONDS( 10 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  // EVERY_N_SECONDS( 5 ) { nextPattern(); } // change patterns periodically
 
 //  if(pir == HIGH){
 //    nextPattern();
 //  }
 }
 
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+// #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
-}
+// void nextPattern()
+// {
+//   // add one to the current pattern number, and wrap around at the end
+//   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+// }
 
-void confetti() 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
-}
+// void confetti() 
+// {
+//   // random colored speckles that blink in and fade smoothly
+//   fadeToBlackBy( leds, NUM_LEDS, 10);
+//   int pos = random16(NUM_LEDS);
+//   leds[pos] += CHSV( gHue + random8(64), 200, 255);
+// }
 
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
-  leds[pos] += CHSV( gHue, 255, 192);
-}
+// void sinelon()
+// {
+//   // a colored dot sweeping back and forth, with fading trails
+//   fadeToBlackBy( leds, NUM_LEDS, 20);
+//   int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+//   leds[pos] += CHSV( gHue, 255, 192);
+// }
 
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
-}
+// void juggle() {
+//   // eight colored dots, weaving in and out of sync with each other
+//   fadeToBlackBy( leds, NUM_LEDS, 20);
+//   byte dothue = 0;
+//   for( int i = 0; i < 8; i++) {
+//     leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+//     dothue += 32;
+//   }
+//  }
